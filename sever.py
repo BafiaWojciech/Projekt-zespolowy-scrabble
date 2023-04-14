fb69f951d32eea770200acef0fadfba567c3c003
Karo <kaarolina.dyme;@gmail.com>
2023-04-14 22:53:57 +0200

    server stub

---
+++ sever.py
@@ -0,0 +1,64 @@
+import socket
+from _thread import *
+import sys
+
+server = '127.0.0.1'
+port = 5556
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0),(100,100)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(str(currentPlayer)))
+    #conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
+        try:
+            print("waiting for data")
+            data = conn.recv(2048).decode()
+            print("recieved data ", data)
+            pos[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
+                print("Sending : ", reply)
+
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    print("waiting for conns")
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
