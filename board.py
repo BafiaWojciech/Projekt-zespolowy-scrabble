e264c1588e34ee17b62f1d38e32eea6f672ec8ca
BafiaWojciech <bafia.wojciech.01@gmail.com>
2023-05-24 18:37:30 +0200

    bugs fixing

--- board.py
+++ board.py
@@ -1,8 +1,9 @@
-import pygame
 from grid import Grid
 from tiles_box import TilesBox
+import pygame
+from tile import Tile
 
-tile_color = (255, 225, 150)
+tile_color = (255, 225, 200)
 
 
 class Board:
@@ -10,6 +11,63 @@ class Board:
         self.rect_size = _rect_size
         self.grid = Grid(25, 25, self.rect_size)
         self.tiles_box = TilesBox(25, 570, self.rect_size)
+        self.rigid_tiles = []
+        self.movable_tiles = []
+
+        self.moved_tile = None
+        self.motion = (0, 0)
+        self.prev_pos = (0, 0)
+        self.dragging = False
+
+        self.font = pygame.font.Font('FreeSansBold.ttf', 26)
+
+    def add_rigid_tile(self, tile):
+        self.rigid_tiles.append(tile)
+
+    def add_movable_tile(self, tile):
+        if len(self.movable_tiles) < 7:
+            for i in range(7):
+                for t in self.movable_tiles:
+                    if t.x == -1 and t.y == i:
+                        break
+                else:
+                    tile.x = -1
+                    tile.y = i
+                    self.movable_tiles.append(tile)
+                    break
+
+    def move(self, _x, _y):
+        if self.dragging:
+            mouse_x, mouse_y = _x, _y
+            self.motion = (self.motion[0] + mouse_x - self.prev_pos[0], self.motion[1] + mouse_y - self.prev_pos[1])
+            self.prev_pos = (_x, _y)
+
+    def drag_tile(self, _x, _y):
+        self.prev_pos = _x, _y
+        x, y = self.get_index(_x, _y)
+        for t in self.movable_tiles:
+            if t.x == x and t.y == y:
+                self.moved_tile = t
+                self.dragging = True
+                return
+
+    def stop_dragging(self, _x, _y):
+        if not self.dragging:       #przy przesuwaniu elementu z "rigid_tiles" bez tego będzie wywalać błąd
+            return
+        final_index = self.get_index(_x, _y)
+        if final_index[0] is not None:
+            found = False
+            for i in self.movable_tiles:
+                if i.x == final_index[0] and i.y == final_index[1]:
+                    found = True
+            for i in self.rigid_tiles:
+                if i.x == final_index[0] and i.y == final_index[1]:
+                    found = True
+            if not found:
+                self.moved_tile.x, self.moved_tile.y = final_index
+        self.moved_tile = None
+        self.dragging = False
+        self.motion = (0, 0)
 
     def get_position(self, _x, _y): #argumenty to pozycja okienka na planszy
         x = y = None
@@ -32,3 +90,20 @@ class Board:
     def draw(self, screen):
         self.grid.draw(screen)
         self.tiles_box.draw(screen)
+
+        for rt in self.rigid_tiles:
+            rect = pygame.Rect(*self.get_position(rt.x, rt.y), self.rect_size - 2, self.rect_size - 2)
+            pygame.draw.rect(screen, tile_color, rect, 0, 6)
+            text = self.font.render(rt.letter, True, (0, 0, 0))
+            screen.blit(text, text.get_rect(center=rect.center))
+
+        for mt in self.movable_tiles:
+            offset = (0, 0)
+            if mt == self.moved_tile:
+                offset = self.motion
+            rect = pygame.Rect(self.get_position(mt.x, mt.y)[0] + offset[0],
+                               self.get_position(mt.x, mt.y)[1] + offset[1],
+                               self.rect_size - 2, self.rect_size - 2)
+            pygame.draw.rect(screen, tile_color, rect, 0, 6)
+            text = self.font.render(mt.letter, True, (0, 0, 0))
+            screen.blit(text, text.get_rect(center=rect.center))
--- main.py
+++ main.py
@@ -12,32 +12,22 @@ start_index = (0, 0)
 prev_pos = (0, 0)
 motion = (0, 0)
 new_index = None
-
+pygame.font.init()
 board = Board(rect_size)
 
 # TilesBag będzie po stronie serwera, tutaj użyte tylko do tego, aby mieć łatwy dostęp do płytek
 tiles_bag = TilesBag()
-# TilesBag losuje pozycje płytek, dlatego tutaj obiektom 'movable_tiles' ręcznie ustawiam pozycje
-movable_tiles = tiles_bag.rand(7)
-print("movable_tiles")
-for i in range(7):
-    movable_tiles[i].x = -1
-    movable_tiles[i].y = i
-    print(movable_tiles[i].letter, movable_tiles[i].x, movable_tiles[i].y)
 
-print("rigid_tiles")
-rigid_tiles = tiles_bag.rand(0)
-for i in rigid_tiles:
-    print(i.letter, i.x, i.y)
+for i in tiles_bag.rand(7):
+    board.add_movable_tile(i)
+
+for i in tiles_bag.rand(10):
+    board.add_rigid_tile(i)
+
 
 pygame.init()
 screen = pygame.display.set_mode([window_width, window_height])
 clock = pygame.time.Clock()
-
-
-font = pygame.font.Font('FreeSansBold.ttf', 26)
-
-
 
 while running:
     for event in pygame.event.get():
@@ -46,49 +36,13 @@ while running:
 
         # dragging
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            index_x, index_y = board.get_index(*event.pos)
-            if index_x is not None:
-                start_index = index_x, index_y
-                dragging = True
-                prev_pos = event.pos
+            board.drag_tile(*event.pos)
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            dragging = False
-            start_position = board.get_position(*start_index)
-            final_position = (start_position[0] + motion[0] + rect_size / 2,
-                              start_position[1] + motion[1] + rect_size / 2)
-            final_index = board.get_index(*final_position)
-            if final_index[0] is not None:
-                new_index = final_index
-            motion = prev_pos = [0, 0]
+            board.stop_dragging(*event.pos)
         elif event.type == pygame.MOUSEMOTION:
-            if dragging:
-                mouse_x, mouse_y = event.pos
-                motion = (motion[0] + mouse_x - prev_pos[0], motion[1] + mouse_y - prev_pos[1])
-                prev_pos = event.pos
+            board.move(*event.pos)
 
-    # board
     board.draw(screen)
-    for rt in rigid_tiles:
-        rect = pygame.Rect(*board.get_position(rt.x, rt.y), rect_size - 2, rect_size - 2)
-        pygame.draw.rect(screen, (255, 225, 150), rect, 0, 6)
-    for mt in movable_tiles:
-        offset = (0, 0)
-        if (mt.x, mt.y) == start_index:
-            if dragging:
-                offset = motion
-            elif new_index is not None:
-                mt.x, mt.y = new_index
-                new_index = None
-                start_index = None
-        rect = pygame.Rect(board.get_position(mt.x, mt.y)[0] + offset[0],
-                           board.get_position(mt.x, mt.y)[1] + offset[1],
-                           rect_size - 2, rect_size - 2)
-        pygame.draw.rect(screen, (255, 255, 250), rect, 0, 6)
-
-        text = font.render(mt.letter, True, (255, 0, 0))
-
-        screen.blit(text, text.get_rect(center = rect.center))
-
     pygame.display.flip()
     clock.tick(FPS)
 
