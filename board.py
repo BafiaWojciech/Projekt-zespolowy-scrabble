86a43ed60f13f4fbd8c68d6f9842ffb61335dff1
BafiaWojciech <bafia.wojciech.01@gmail.com>
2023-05-31 14:19:56 +0200

    exchange tiles

--- board.py
+++ board.py
@@ -11,75 +11,32 @@ from tiles_bag import *
 tile_color = (247, 247, 215)
 
 points_color = {
-                0: (170, 170, 170),
-                1: (196, 204, 90),
-                2: (145, 204, 90),
-                3: (15, 150, 49),
-                4: (37, 193, 196),
-                5: (37, 95, 196),
-                6: (237, 133, 36),
-                7: (240, 65, 22),
-                9: (250, 12, 0)
-            }
+    0: (170, 170, 170),
+    1: (196, 204, 90),
+    2: (145, 204, 90),
+    3: (15, 150, 49),
+    4: (37, 193, 196),
+    5: (37, 95, 196),
+    6: (237, 133, 36),
+    7: (240, 65, 22),
+    9: (250, 12, 0)
+}
 
 points_for_letter = {
-    'A': 1,
-    'Ą': 5,
-    'B': 3,
-    'C': 2,
-    'Ć': 6,
-    'D': 2,
-    'E': 1,
-    'Ę': 5,
-    'F': 5,
-    'G': 3,
-    'H': 3,
-    'I': 1,
-    'J': 3,
-    'K': 2,
-    'L': 2,
-    'Ł': 3,
-    'M': 2,
-    'N': 1,
-    'Ń': 7,
-    'O': 1,
-    'Ó': 5,
-    'P': 2,
-    'R': 1,
-    'S': 1,
-    'Ś': 5,
-    'T': 2,
-    'U': 3,
-    'W': 1,
-    'Y': 2,
-    'Z': 1,
-    'Ź': 9,
-    'Ż': 5,
-    ' ': 0
+    'A': 1, 'Ą': 5, 'B': 3, 'C': 2, 'Ć': 6, 'D': 2, 'E': 1, 'Ę': 5, 'F': 5, 'G': 3, 'H': 3, 'I': 1, 'J': 3, 'K': 2,
+    'L': 2, 'Ł': 3, 'M': 2, 'N': 1, 'Ń': 7, 'O': 1, 'Ó': 5, 'P': 2, 'R': 1, 'S': 1, 'Ś': 5, 'T': 2, 'U': 3, 'W': 1,
+    'Y': 2, 'Z': 1, 'Ź': 9, 'Ż': 5, ' ': 0
 }
 
 bonsu_field = {
-    (0, 0): "W3", (0, 7): "W3", (0, 14): "W3",
-    (7, 0): "W3", (7, 14): "W3", (14, 0): "W3",
-    (14, 7): "W3", (14, 14): "W3", (1, 1): "W2",
-    (2, 2): "W2", (3, 3): "W2", (4, 4): "W2",
-    (10, 4): "W2", (11, 3): "W2", (12, 2): "W2",
-    (13, 1): "W2", (4, 10): "W2", (3, 11): "W2",
-    (2, 12): "W2", (1, 13): "W2", (10, 10): "W2",
-    (11, 11): "W2", (12, 12): "W2", (13, 13): "W2",
-    (7, 7): "W2", (1, 5): "L3", (5, 1): "L3",
-    (1, 9): "L3", (9, 1): "L3", (5, 5): "L3",
-    (9, 5): "L3", (5, 9): "L3", (13, 5): "L3",
-    (9, 9): "L3", (5, 13): "L3", (13, 9): "L3",
-    (9, 13): "L3", (3, 0): "L2", (0, 3): "L2",
-    (6, 2): "L2", (2, 6): "L2", (7, 3): "L2",
-    (3, 7): "L2", (8, 2): "L2", (2, 8): "L2",
-    (11, 0): "L2", (0, 11): "L2", (6, 6): "L2",
-    (3, 14): "L2", (14, 3): "L2", (8, 6): "L2",
-    (6, 8): "L2", (8, 8): "L2", (12, 6): "L2",
-    (6, 12): "L2", (11, 7): "L2", (7, 11): "L2",
-    (12, 8): "L2", (8, 12): "L2", (14, 11): "L2",
-    (11, 14): "L2"
+    (0, 0): "W3", (0, 7): "W3", (0, 14): "W3", (7, 0): "W3", (7, 14): "W3", (14, 0): "W3", (14, 7): "W3",
+    (14, 14): "W3", (1, 1): "W2", (2, 2): "W2", (3, 3): "W2", (4, 4): "W2", (10, 4): "W2", (11, 3): "W2", (12, 2): "W2",
+    (13, 1): "W2", (4, 10): "W2", (3, 11): "W2", (2, 12): "W2", (1, 13): "W2", (10, 10): "W2", (11, 11): "W2",
+    (12, 12): "W2", (13, 13): "W2", (7, 7): "W2", (1, 5): "L3", (5, 1): "L3", (1, 9): "L3", (9, 1): "L3", (5, 5): "L3",
+    (9, 5): "L3", (5, 9): "L3", (13, 5): "L3", (9, 9): "L3", (5, 13): "L3", (13, 9): "L3", (9, 13): "L3", (3, 0): "L2",
+    (0, 3): "L2", (6, 2): "L2", (2, 6): "L2", (7, 3): "L2", (3, 7): "L2", (8, 2): "L2", (2, 8): "L2", (11, 0): "L2",
+    (0, 11): "L2", (6, 6): "L2", (3, 14): "L2", (14, 3): "L2", (8, 6): "L2", (6, 8): "L2", (8, 8): "L2", (12, 6): "L2",
+    (6, 12): "L2", (11, 7): "L2", (7, 11): "L2", (12, 8): "L2", (8, 12): "L2", (14, 11): "L2", (11, 14): "L2"
 }
 
 class Board:
@@ -104,12 +61,12 @@ class Board:
         self.send_button = Button("wyślij", 375, 570, 80, 35)
         self.pass_button = Button("pasuj", 470, 570, 80, 35)
 
-
         self.points_info = ""
         self.total_points_info = ""
         self.err_info = ""
         self.pick_letter = False
         self.letter_picker = LetterPicker()
+        self.exchange_letter_confirmation = False
 
     def draw(self, screen):
         self.grid.draw(screen)
@@ -121,7 +78,7 @@ class Board:
         for rt in self.rigid_tiles:
             rect = pygame.Rect(*self.get_position(rt.x, rt.y), self.rect_size - 2, self.rect_size - 2)
             pygame.draw.rect(screen, points_color[points_for_letter[rt.letter[0]]], rect, 0, 6)
-            text = self.font.render(rt.letter[-1], True, (255, 255, 255))
+            text = self.font.render(rt.letter[-1], True, (0, 0, 0))
             screen.blit(text, text.get_rect(center=rect.center))
 
         for mt in self.movable_tiles:
@@ -146,6 +103,7 @@ class Board:
 
         if self.pick_letter:
             self.letter_picker.draw(screen)
+
 
     def add_rigid_tile(self, tile):
         self.rigid_tiles.append(tile)
@@ -174,6 +132,18 @@ class Board:
         else:
             self.drag_tile(_x, _y)
             self.total_points_info = "punkty: " + str(self.total_points)
+            if self.swap_button.check_click(_x, _y):
+                tmp = [t for t in self.movable_tiles if t.x != -1]
+                if len(tmp) != 0 and self.exchange_letter_confirmation:
+                    #TODO - wysłać litery do wymiany na serwer, usunąć je z self.movable_tiles i wykonać funkcję self.add_movable_tile
+                    print("litery do wymiany")
+                    for t in tmp:
+                        print(t.letter, t.x, t.y)
+                elif not self.exchange_letter_confirmation:
+                    self.exchange_letter_confirmation = True
+            else:
+                self.err_info = ""
+                self.exchange_letter_confirmation = False
             if self.send_button.check_click(_x, _y):
                 if self.validate_board_placement():
                     self.err_info = ""
@@ -182,12 +152,12 @@ class Board:
                             self.err_info = "nie ma takiego słowa jak: " + w
                             return
 
-                    self.total_points += self.points_in_turn
-                    self.total_points_info = "punkty: " + str(self.total_points)
-                    print("slowa do wyslania na serwer:")
+                    print("ułożone słowa:")
                     for s in self.words_in_turn:
                         print(s)
 
+                    # TODO - serwer musi odebrać litery wykonane podczas ruchu oraz liczbę punktów za ruch
+                    #  (zmienna self.points_in_turn) żeby rozesłać je do innych przeciwników
                     print("litery do wysłania na serwer:")
                     tmp = [t for t in self.movable_tiles if t.x != -1]
                     for t in tmp:
@@ -195,21 +165,23 @@ class Board:
                         self.rigid_tiles.append(t)
                         self.movable_tiles.remove(t)
 
-                    # w tym miejscu z serwera powinniśmy dostać wylosowane literki
+                    # TODO - tutaj gracz powinien dostać literki z serwera, zamiast generować losować je tak jak poniżej
                     tiles_bag = TilesBag()
                     for i in tiles_bag.rand(len(tmp)):
                         self.add_movable_tile(i)
 
-
-            if self.swap_button.check_click(_x, _y):
-                print("wymień")
+                    self.total_points += self.points_in_turn
+                    self.points_in_turn = 0
+                    self.total_points_info = "punkty: " + str(self.total_points)
 
             if self.pass_button.check_click(_x, _y):
-                print("pasuj")
                 for i, t in enumerate(self.movable_tiles):
                     t.letter = t.letter[0]
                     t.x = -1
                     t.y = i
+                # TODO - wysłanie informacji o pasowaniu tury do serwera
+                #  WAŻNE - występuje taka zasada, że jeżeli każdy gracz spasuje dwa razy pod rząd to gra się kończy
+                print("pasuj")
 
     def mouse_button_up(self, _x, _y):
         self.stop_dragging(_x, _y)
@@ -314,7 +286,7 @@ class Board:
         perpendicular_words_points = 0
         total_word_bonus = 1
 
-        #if len(tmp) != 1:
+        # if len(tmp) != 1:
         min_x = min(tmp, key=lambda tmp: tmp.x)
         min_y = min(tmp, key=lambda tmp: tmp.y)
         max_x = max(tmp, key=lambda tmp: tmp.x)
@@ -329,16 +301,12 @@ class Board:
             while bottom.y + 1 < len(board[bottom.x]) and board[bottom.x][bottom.y + 1] is not None:
                 bottom = board[bottom.x][bottom.y + 1]
             for i in range(top.y, bottom.y + 1):
-
-                if board[top.x][i] is None:
-                    self.err_info = "nie ma ciągłości w pionie a"
-                    return False
-                else:           # liczenie punktów
+                if isinstance(board[top.x][i], Tile):
                     words[0].append(board[top.x][i].letter)
                     l = board[top.x][i]
                     points += points_for_letter[l.letter[0]]
                     for t in tmp:
-                        if (t.x, t.y) == (l.x, l.y):    #jeżeli litera jest z movabletiles na planszy
+                        if (t.x, t.y) == (l.x, l.y):
                             left = l
                             right = l
                             while board[left.x - 1][left.y] is not None:
@@ -352,7 +320,7 @@ class Board:
                                 a = board[i][left.y]
                                 tmp_points += points_for_letter[a.letter[0]]
                             tmp_points = tmp_points - points_for_letter[l.letter[0]]
-                            bonus = bonsu_field.get((l.x,l.y), " 1")
+                            bonus = bonsu_field.get((l.x, l.y), " 1")
                             if bonus[0] == 'W':
                                 tmp_points *= int(bonus[1])
                                 total_word_bonus *= int(bonus[1])
@@ -360,6 +328,9 @@ class Board:
                                 points += (points_for_letter[l.letter[0]] * ((int(bonus[1])) - 1))
                             perpendicular_words_points += tmp_points
                             break
+                else:
+                        self.err_info = "nie ma ciągłości w pionie"
+                        return False
 
         elif min_y == max_y:  # poziomo
             left = min_x
@@ -369,15 +340,12 @@ class Board:
             while right.x + 1 < len(board) and board[right.x + 1][right.y] is not None:
                 right = board[right.x + 1][right.y]
             for i in range(left.x, right.x + 1):
-                if board[i][left.y] is None:
-                    self.err_info = "nie ma ciągłości w poziomie b"
-                    return False
-                else:           # liczenie punktów
+                if isinstance(board[i][left.y], Tile):
                     words[0].append(board[i][left.y].letter)
                     l = board[i][left.y]
                     points += points_for_letter[l.letter[0]]
                     for t in tmp:
-                        if (t.x, t.y) == (l.x, l.y):    #jeżeli litera jest nowa
+                        if (t.x, t.y) == (l.x, l.y):
                             top = l
                             bottom = l
                             while board[top.x][top.y - 1] is not None:
@@ -400,7 +368,9 @@ class Board:
                                 points += (points_for_letter[l.letter[0]] * ((int(bonus[1])) - 1))
                             perpendicular_words_points += tmp_points
                             break
-
+                else:
+                    self.err_info = "nie ma ciągłości w poziomie"
+                    return False
         else:
             self.err_info = "nie w jednej linii"
             return False
@@ -421,6 +391,7 @@ class Board:
         points += perpendicular_words_points
         if len(tmp) == 7:
             points += 50
+
         self.points_in_turn = points
         self.points_info = "punkty za ruch: " + str(self.points_in_turn)
         return True
@@ -434,3 +405,6 @@ class Board:
             if soup.find("body").find("p").text == "dopuszczalne w grach (i) ":
                 return True
         return False
+
+
+#TODO - dodatkowe info - na końcu serwer musi poinformować graczy o zakończeniu gry
